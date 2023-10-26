238424b2059199c44126d103ec0f994c12aeb7de
DmytroD1o <136333833+DmytroD1o@users.noreply.github.com>
2023-10-26 15:44:25 +0000

    Deploying to gh-pages from @ DmytroD1o/goit-js-hw-09@4ce3acaa1372907819bf2a45ca46b9defa55073e 🚀

---
+++ 01-color-switcher.30445e32.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let n=null;t.addEventListener("click",(function(){n=setInterval((()=>{t.disabled=!0;const e=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.backgroundColor=e}),1e3)})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.30445e32.js.map
